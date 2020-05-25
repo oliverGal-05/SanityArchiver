f9e23b7a6f452c7ff53ca29f2a68bf3045ae5346
arpadtorzsok <arpad.torzsok@codecool.com>
2020-05-25 13:16:59 +0200

    Initial commit

---
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace SanityArchiver.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
