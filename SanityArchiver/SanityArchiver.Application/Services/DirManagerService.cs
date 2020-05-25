ca6b42578c026baba983e60df90981c1c4991e81
oliverGal-05 <galoliver05@gmail.com>
2020-05-26 01:32:11 +0200

    1st user story 4/5 done, missing a conversion (file size)

---
+++ SanityArchiver/SanityArchiver.Application/Services/DirManagerService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SanityArchiver.Application.Models;
+
+namespace SanityArchiver.Application.Services
+{
+    /// <summary>
+    /// GetDrives
+    /// </summary>
+    public class DirManagerService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirManagerService"/> class.
+        /// </summary>
+        /// <returns>All drives in <see cref="DirManagerModel"/></returns>
+        public List<DirManagerModel> GetAllDrives()
+        {
+            List<DirManagerModel> dirManagerModels = new List<DirManagerModel>();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    dirManagerModels.Add(new DirManagerModel(drive.RootDirectory.FullName));
+                }
+            }
+
+            return dirManagerModels;
+        }
+    }
+}
--- SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using SanityArchiver.DesktopUI.ViewModels;
+using SanityArchiver.Application.Models;
 
 namespace SanityArchiver.DesktopUI.Views
 {
@@ -13,6 +17,27 @@ namespace SanityArchiver.DesktopUI.Views
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowVM = new MainWindowViewModel();
+            DataContext = MainWindowVM;
+        }
+
+        /// <summary>
+        /// Gets or sets MainVM
+        /// </summary>
+        public MainWindowViewModel MainWindowVM { get; set; }
+
+        private void TreeView_PreviewMouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var depObj = (DependencyObject)e.OriginalSource;
+            while ((depObj != null) && !(depObj.GetType().Name is "TreeViewItem"))
+            {
+                depObj = VisualTreeHelper.GetParent(depObj);
+            }
+
+            if ((DependencyObject)e.OriginalSource is TextBlock)
+            {
+                MainWindowVM.DisplayCurrentFiles((DirManagerModel)((TreeViewItem)depObj).Header);
+            }
         }
     }
 }
