e40699ae56c76b38ffb6ee91089b0ee01eab4af5
Rhyvan <polyakan@gmail.com>
2020-05-26 14:20:21 +0200

    add FileAttribute Dialog and ViewModel (readonly)

---
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/FileAttributeViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace SanityArchiver.DesktopUI.ViewModels
+{
+    /// <summary>
+    /// Handles properties and methods binded to FileAttributeDialog view.
+    /// </summary>
+    public class FileAttributeViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileAttributeViewModel"/> class.
+        /// </summary>
+        /// <param name="file">FileInfo</param>
+        public FileAttributeViewModel(string file)
+        {
+            File = new FileInfo(file);
+            FilteredFile = new MyFile(File);
+        }
+
+        /// <summary>
+        /// Gets or sets File.
+        /// </summary>
+        public FileInfo File { get; set; }
+
+        /// <summary>
+        /// Gets or Sets FilteredFile.
+        /// </summary>
+        public MyFile FilteredFile { get; set; }
+
+        /// <summary>
+        /// Inner helper class to store different File properties and attributes.
+        /// </summary>
+        public class MyFile
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MyFile"/> class.
+            /// </summary>
+            /// <param name="file">Used to initialize properties.</param>
+            public MyFile(FileInfo file)
+            {
+                Name = Path.GetFileNameWithoutExtension(file.FullName);
+                Extension = file.Extension;
+                Hidden = file.Attributes.HasFlag(FileAttributes.Hidden);
+            }
+
+            /// <summary>
+            /// Gets the Name.
+            /// </summary>
+            public string Name { get; private set; }
+
+            /// <summary>
+            /// Gets Extension
+            /// </summary>
+            public object Extension { get; private set; }
+
+            /// <summary>
+            /// Gets or sets a value indicating whether is hidden or not.
+            /// </summary>
+            public bool Hidden { get; set; }
+        }
+    }
+}
---
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/FileAttributeDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using SanityArchiver.DesktopUI.ViewModels;
+
+namespace SanityArchiver.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for FileAttributeDialog.xaml
+    /// </summary>
+    public partial class FileAttributeDialog : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileAttributeDialog"/> class.
+        /// </summary>
+        /// <param name="path">Full path of file.</param>
+        public FileAttributeDialog(string path)
+        {
+            InitializeComponent();
+            FileAttributeViewModel vm = new FileAttributeViewModel(path);
+            DataContext = vm.FilteredFile;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
